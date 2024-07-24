1dc7ec36c06c15f9033105a07e15dfb3fd82634b
lucaserib <lucasemanuelribeiro@hotmail.com>
2024-07-24 16:48:24 -0300

    Hotfix e scale

--- src/main/java/me/dio/domain/model/Card.java
+++ src/main/java/me/dio/domain/model/Card.java
@@ -14,7 +14,7 @@ public class Card {
     @Column(unique = true)
     private String number;
 
-    @Column(name = "available_limit", scale = 13, precision = 2)
+    @Column(name = "available_limit", precision =   2, scale = 13)
     private BigDecimal limit;
 
     public Long getId() {
